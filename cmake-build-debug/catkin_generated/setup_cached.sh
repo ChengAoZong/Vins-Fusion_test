a41c91aff488bd35f48939b06a70167991d1d1c3
chengaozong <864224983@qq.com>
2022-04-11 12:02:50 +0800

    test

---
+++ cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/chengaozong/catkin_ws/src/learning_service/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export PYTHONPATH='/home/chengaozong/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages:/home/chengaozong/catkin_ws/devel/lib/python3/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/chengaozong/catkin_ws/src/learning_service/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/chengaozong/catkin_ws/src/learning_service:$ROS_PACKAGE_PATH"
